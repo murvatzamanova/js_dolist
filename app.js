14186d945dc8f9f0a129ea01de9dffbdefce97be
murvatzamanova <zamanova0504@gmail.com>
2024-04-10 19:44:24 +0400

    jstasks

---
+++ app.js
@@ -0,0 +1,17 @@
+const tbody = document.querySelector("#data");
+const table = document.querySelector("#table");
+let data = document.querySelector("#data"); 
+form.addEventListener("click", () => {
+    document.getElementById("plusButton").addEventListener("click", function() {
+        data.append
+        });
+        document.getElementById('savebutton').addEventListener('click', function() {
+            data.map((melumat) =>{
+                data.append = document.getElementById('data');
+                datagoster.textContent = data;
+            });
+        
+          });
+});
+  data.innerHTML = "";
+  data.append
